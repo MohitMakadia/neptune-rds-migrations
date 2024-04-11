a5d69a991133c1ce3dd2e272f1dd22fb4aa35eb1
MohitMakadia <mohitmakadia123@gmail.com>
2024-04-11 17:40:21 +0530

    Added requirement.txt

--- migrate.py
+++ migrate.py
@@ -91,42 +91,51 @@ class RDS:
         with createRdsSession() as session:
             vertedIds = self.g.V().hasLabel("worker").toList()
             for vertedId in vertedIds:
-                workerValueMaps = self.g.V(vertedId).valueMap().toList()
-                for workerValueMap in workerValueMaps:
-                    Base.metadata.bind = self.engine
-                    session = createRdsSession()
-                    worker = Worker(
-                        worker_id = vertedId.id,
-                        amount = workerValueMap.get("amount", [0])[0],
-                        domain_language = workerValueMap.get("domain_language", [""])[0],
-                        domain = workerValueMap.get("domain", [""])[0],
-                        email = workerValueMap.get("email", [""])[0],
-                        experience = workerValueMap.get("experience", [0])[0],
-                        first_name = workerValueMap.get("first_name", [""])[0],
-                        internal_score = workerValueMap.get("internal_score", [0])[0],
-                        is_online = workerValueMap.get("is_online", [False])[0],
-                        is_suspended = workerValueMap.get("is_suspended", [False])[0],
-                        last_login = workerValueMap.get("last_login", [0])[0],
-                        location_address = workerValueMap.get("location_address", [""])[0],
-                        location_city = workerValueMap.get("location_city", [""])[0],
-                        location_country = workerValueMap.get("location_country", [""])[0],
-                        location_longitude = workerValueMap.get("location_longitude", [0.0])[0],
-                        location_latitude = workerValueMap.get("location_latitude", [0.0])[0],
-                        location_place_id = workerValueMap.get("location_place_id", [""])[0],
-                        location_slug = workerValueMap.get("location_slug", [""])[0],
-                        max_distance = workerValueMap.get("max_distance", [0])[0],
-                        personal_note = workerValueMap.get("personal_note", [""])[0],
-                        profile_picture = workerValueMap.get("profile_picture", [""])[0],
-                        published = workerValueMap.get("published", [False])[0],
-                        registered_date = workerValueMap.get("registered_date", [0])[0],
-                        score = workerValueMap.get("score", [0])[0],
-                        user_alert = workerValueMap.get("user_alert", [False])[0],
-                        votes = workerValueMap.get("votes", [0])[0],
-                        )
-                    session.add(worker)
-                    commitRds(session)
+                if self.validate_uuid(vertedId.id):
+                    workerValueMaps = self.g.V(vertedId).valueMap().toList()
+                    for workerValueMap in workerValueMaps:
+                        Base.metadata.bind = self.engine
+                        session = createRdsSession()
+                        try:
+                            worker = Worker(
+                                worker_id = vertedId.id,
+                                amount = workerValueMap.get("amount", [0])[0],
+                                domain_language = workerValueMap.get("domain_language", [""])[0],
+                                domain = workerValueMap.get("domain", [""])[0],
+                                email = workerValueMap.get("email", [""])[0],
+                                experience = workerValueMap.get("experience", [0])[0],
+                                first_name = workerValueMap.get("first_name", [""])[0],
+                                internal_score = workerValueMap.get("internal_score", [0])[0],
+                                is_online = workerValueMap.get("is_online", [False])[0],
+                                is_suspended = workerValueMap.get("is_suspended", [False])[0],
+                                last_login = workerValueMap.get("last_login", [0])[0],
+                                location_address = workerValueMap.get("location_address", [""])[0],
+                                location_city = workerValueMap.get("location_city", [""])[0],
+                                location_country = workerValueMap.get("location_country", [""])[0],
+                                location_longitude = workerValueMap.get("location_longitude", [0.0])[0],
+                                location_latitude = workerValueMap.get("location_latitude", [0.0])[0],
+                                location_place_id = workerValueMap.get("location_place_id", [""])[0],
+                                location_slug = workerValueMap.get("location_slug", [""])[0],
+                                max_distance = workerValueMap.get("max_distance", [0])[0],
+                                personal_note = workerValueMap.get("personal_note", [""])[0],
+                                profile_picture = workerValueMap.get("profile_picture", [""])[0],
+                                published = workerValueMap.get("published", [False])[0],
+                                registered_date = workerValueMap.get("registered_date", [0])[0],
+                                score = workerValueMap.get("score", [0])[0],
+                                user_alert = workerValueMap.get("user_alert", [False])[0],
+                                votes = workerValueMap.get("votes", [0])[0],
+                                )
+                            session.add(worker)
+                            commitRds(session)
+                        except Exception as e:
+                            print(str(e))
+                            session.close()
 
-rds = RDS("Customer")
+                else:
+                    print(f'Invalid UUID Detected {vertedId.id} ... Skipping.')
+
+
+rds = RDS("Worker")
 # rds.migrateCustomer()
 
 
