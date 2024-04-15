5e0020c6a5e95480a79585df99ddb2fff63cf822
MohitMakadia <mohitmakadia123@gmail.com>
2024-04-15 19:18:11 +0530

    Added message

---
+++ message.py
@@ -0,0 +1,65 @@
+# type: ignore[import]
+from models.Message import Message, Base
+from utils.connect import rdsConnect, neptuneConnect
+from gremlin_python.structure.graph import Graph
+from utils.session import createRdsSession, commitRds
+from utils.validation import validate_uuid, checkIfTableExists
+ 
+class migrateMessage:
+
+    def __init__(self):
+        self.engine = rdsConnect()
+        self.neptune_engine = neptuneConnect()
+        self.g = Graph().traversal().withRemote(self.neptune_engine)
+        self.table = "Message"
+
+    def createCustomerTable(self):
+        print(f'Creating {self.table} Table ...')
+        Message.tableLaunch()
+        print(f'{self.table} Table Created')
+
+    def migrateMessage(self):
+        checkIfTableExists(self.engine, self.table)
+        self.createCustomerTable()
+        print(f'Starting Migration for {self.table} table ...')
+        vertexIds = [v.id for v in self.g.V().hasLabel("message").toList()]
+        vertexIterate = iter(vertexIds)
+        while True:
+            try:
+                vertexId = next(vertexIterate)
+                if validate_uuid(vertexId):
+                    Base.metadata.bind = self.engine
+                    messageValueMap = self.g.V(vertexId).valueMap().toList()[0]
+                    outEdges = self.g.V(vertexId).outE().toList()
+                    for edge in outEdges:
+                        try:
+                            session = createRdsSession()
+                            message = Message(
+                                message_id = vertexId,
+                                content = messageValueMap.get("content", [""])[0],
+                                last_login = messageValueMap.get("last_login", [0])[0],
+                                seen = messageValueMap.get("seen", ["False"])[0],
+                                sent_timestamp = messageValueMap.get("sent_timestamp", [0])[0],
+                                authored_by = None,
+                                addressed_to = None,
+                                is_part_of = None
+                            )
+                            if edge.label == "authored_by":
+                                message.authored_by = edge.id
+                            elif edge.label == "addressed_to":
+                                message.addressed_to = edge.id
+                            elif edge.label == "is_part_of":
+                                message.is_part_of = edge.id
+                            
+                            session.add(message)
+                            commitRds(session)
+
+                        except Exception as e:
+                            print(f'Failed due to {str(e)}')  
+                else:
+                    print(f'Invalid UUID Detected {vertexId} ... Skipping.')
+            except StopIteration:
+                break
+                
+currency = migrateMessage()
+currency.migrateMessage()
---
+++ models/Message.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.dialects.postgresql import UUID as PGUUID
+from utils.connect import rdsConnect
+from utils.session import commitRds, deployTable
+
+Base = declarative_base()
+engine = rdsConnect()
+
+class Message(Base):
+    
+    __tablename__ = "Message"
+
+    id = Column(Integer, primary_key=True)
+    message_id = Column(PGUUID(as_uuid=True))
+    content = Column(String(1000))
+    last_login = Column(Integer())
+    seen = Column(Boolean, default=False)
+    sent_timestamp = Column(Integer())
+    authored_by = Column(PGUUID(as_uuid=True))
+    addressed_to = Column(PGUUID(as_uuid=True))
+    is_part_of = Column(PGUUID(as_uuid=True))
+
+    def tableLaunch():
+        commitRds(deployTable(Base, engine))
